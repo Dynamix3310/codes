cc608451fbfe9cc5bf0901670ac89944dd5eb408
eggeggwe <westinlai940221@gmail.com>
2026-09-15 20:54:22 +0800

    data structure change

--- data_structure/sparse_table.cpp
+++ data_structure/sparse_table.cpp
@@ -1,19 +1,25 @@
-struct sparse_table{
-    vector<vector<long long int> > table;
+struct sparse_table {
+    vector<vector<long long int>> table;
     int n;
-    void init(int N,vector<long long int> &a){
-        n=N;
-        int k=__lg(n)+1;
-        table = vector<vector<long long int> >(k+1,vector<long long int>(n,0));
-        table[0]=a;
-        for(int j=1;j<=k;j++){
-            for(int i=0;i<n;i++){
-                table[j][i]=min(table[j-1][i],table[j-1][min(n-1,i+(1<<(j-1)))]);
+
+    void init(int N, const vector<long long int> &a) {
+        n = N;
+        if (n <= 0) return;
+        int k = __lg(n);
+        table.assign(k + 1, vector<long long int>(n));
+        table[0] = a;
+
+        for (int j = 1; j <= k; j++) {
+            int step = 1 << (j - 1);
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                table[j][i] = min(table[j - 1][i], table[j - 1][i + step]);
             }
         }
     }
-    long long int query(int l,int r){
-        int j=__lg(r-l+1);
-        return min(table[j][l],table[j][r-(1<<j)+1]);
+
+    // 需確保 0 <= l <= r < n
+    long long int query(int l, int r) {
+        int j = __lg(r - l + 1);
+        return min(table[j][l], table[j][r - (1 << j) + 1]);
     }
 };
--- data_structure/基環dsu.cpp
+++ data_structure/基環dsu.cpp
@@ -1,34 +1,41 @@
 //允許連通兩次的dsu，圖會是基環森林
-struct dsu{
-    struct node{
+struct dsu {
+    struct node {
         int boss;
-        bool cyc=0;
+        int sz = 1;
+        bool cyc = false;
     };
     vector<node> tree;
-    ll n;
-    void init(int N){
-        n=N;
-        tree.assign(n+10,node());
-        rep(i,1,n+1)tree[i].boss=i;
+    int n;
+
+    void init(int N) {
+        n = N;
+        tree.assign(n + 10, node());
+        for (int i = 0; i <= n; i++) tree[i].boss = i; // 支援 0-indexed 與 1-indexed
     }
-    int boss(int x){
-        if(tree[x].boss==x)return x;
-        tree[x].boss=boss(tree[x].boss);
-        return tree[x].boss;
+
+    int boss(int x) {
+        if (tree[x].boss == x) return x;
+        return tree[x].boss = boss(tree[x].boss);
     }
-    bool merge(int u,int v){
-        int U=boss(u);
-        int V=boss(v);
-        if(U==V){
-            if(tree[U].cyc==0){
-                tree[U].cyc=1;
+
+    bool merge(int u, int v) {
+        int U = boss(u);
+        int V = boss(v);
+        if (U == V) {
+            if (!tree[U].cyc) {
+                tree[U].cyc = true;
                 return true;
             }
             return false;
         }
-        if(tree[U].cyc && tree[V].cyc)return false;
-        if(tree[U].cyc==1)swap(U,V);
-        tree[U].boss=V;
+        if (tree[U].cyc && tree[V].cyc) return false;
+
+        // 啟發式合併：將小樹掛到大樹下
+        if (tree[U].sz > tree[V].sz) swap(U, V);
+        tree[U].boss = V;
+        tree[V].sz += tree[U].sz;
+        tree[V].cyc |= tree[U].cyc; // 解耦狀態轉移與樹的合併方向
         return true;
     }
 };
